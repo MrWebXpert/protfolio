925e391a74009994ad65c1a47a1fe3ba15d62bf7
MrWebXpert <mrwebxpert@gmail.com>
2024-03-18 06:32:35 +0500

    changing in content and adding footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,16 +4,16 @@ import { NavLink } from 'react-router-dom'
 const Footer = () => {
     return (
         <>
-            <div className="container">
+            <div className="container mt-5 ">
                 <footer className="py-3 my-4">
-                    <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-                        <li className="nav-item"><NavLink to="#home" className="nav-link px-2 text-body-secondary">Home</NavLink></li>
-                        <li className="nav-item"><NavLink to="#about" className="nav-link px-2 text-body-secondary">About</NavLink></li>
-                        <li className="nav-item"><NavLink to="#services" className="nav-link px-2 text-body-secondary">Services</NavLink></li>
-                        <li className="nav-item"><NavLink to="#porfolio" className="nav-link px-2 text-body-secondary">Portfolio</NavLink></li>
-                        <li className="nav-item"><NavLink to="#contact" className="nav-link px-2 text-body-secondary">Contact</NavLink></li>
+                    <ul className="nav justify-content-center border-bottom pb-3 mb-3 ">
+                        <li className="nav-item"><a href="#home" className="nav-link px-2 ">Home</a></li>
+                        <li className="nav-item"><a href="#about" className="nav-link px-2 ">About</a></li>
+                        <li className="nav-item"><a href="#services" className="nav-link px-2 ">Services</a></li>
+                        <li className="nav-item"><a href="#porfolio" className="nav-link px-2 ">Portfolio</a></li>
+                        <li className="nav-item"><a href="#contact" className="nav-link px-2y">Contact</a></li>
                     </ul>
-                    <p className="text-center text-body-secondary">© 2024 Company, copyright All right reserved</p>
+                    <p className="text-center ">© 2024 Company, copyright All right reserved</p>
                 </footer>
             </div></>
     )
